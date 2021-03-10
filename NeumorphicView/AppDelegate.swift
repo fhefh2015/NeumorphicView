4036dc47c9b1853084ab7a8368fe86b23924646b
Smith <apple@Macs-iMac.local>
2021-03-10 16:06:44 +0800

    exit the app

--- NeumorphicView/AppDelegate.swift
+++ NeumorphicView/AppDelegate.swift
@@ -15,10 +15,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        NotificationCenter.default.addObserver(self, selector: #selector(self.windowClose(_:)), name: NSWindow.willCloseNotification, object: nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+
+    @objc func windowClose(_ aNotification: Notification) {
+
+        if let window = aNotification.object as? NSWindow {
+
+            NSApp.terminate(window)
+        }
+
     }
 
 
